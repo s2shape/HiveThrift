6c2c2f90272f618fdac36d4b6040cafecf1fcb34
Mikhail Burilov <burilovmv@gmail.com>
2019-09-03 19:22:17 +0300

    Converted to .net core

--- HiveThrift/Example/Program.cs
+++ HiveThrift/Example/Program.cs
@@ -1,10 +1,10 @@
-﻿using Hive2;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Hive2;
 
 namespace Example
 {
-    internal class Program
+    class Program
     {
         private static void Main(string[] args)
         {
@@ -51,26 +51,5 @@ namespace Example
             Console.ReadLine();
         }
 
-        //public static void Execute(HiveClient client, string query)
-        //{
-        //    try
-        //    {
-        //        var operation = client.ExecuteStatement(query);
-        //        var resultsSet = client.FetchResult(operation);
-        //        foreach (var item in resultsSet.Columns)
-        //        {
-        //            if (item.StringVal.Values != null && item.StringVal.Values.Count > 0)
-        //                foreach (var info in item.StringVal.Values)
-        //                {
-        //                    Console.WriteLine(info);
-        //                }
-        //        }
-        //        client.CloseOperation(operation);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //    }
-        //}
     }
 }
--- README.md
+++ README.md
@@ -1,15 +1,4 @@
 ﻿# HiveThrift
 
-本项目是c#版hive thrift 的简单封装
+.Net Standard library for Hive using Thrift protocol
 
-## support 
-
-本项目可以支持访问 hive 的 thrift hive server 和 thrift hive server 2
-
-由于spark sql 采用 thrift hive server 2， 所以本项目也支持 spark sql 
-
-spark sql 的例子：../HiveThrift/Example/Program.cs
-
-## Nuget
-
-https://www.nuget.org/packages/Gridsum.HiveThrift/0.1.3
